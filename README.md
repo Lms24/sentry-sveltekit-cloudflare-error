8946803b56231cb3b02e08735aeb33be61ceb1bd
Lukas Stracke <lukas.stracke@sentry.io>
2024-10-15 13:30:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sentry + Cloudflare Reproduction
+
+Reproduce build error:
+
+1. `npm install`
+2. `npm run build`
+3. observe "Could not resolve $app/stores" build error of adapter-cloudflare
---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://27dbca58093d401f9b18f88c40ec718f@o447951.ingest.us.sentry.io/4504796902588416',
+
+  tracesSampleRate: 1.0,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
